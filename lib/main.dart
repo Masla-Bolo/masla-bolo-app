7491cc07a21a35efc5d4ff017d10d4bcb540e9ab
Asher <asheressani@gmail.com>
2024-11-28 22:45:52 +0500

    Added Event Bus!

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
-import 'package:event_bus/event_bus.dart';
 import 'package:flutter/material.dart';
 import 'package:masla_bolo_app/initializer.dart';
 
+import 'helpers/event_bus.dart';
 import 'presentation/masla_bolo.dart';
 
 void main() {
