9dcaa508044bb5fe6282a1159f017571514de89c
Asher <asheressani@gmail.com>
2025-07-20 18:04:48 +0500

    Finalized App

--- lib/helpers/strings.dart
+++ lib/helpers/strings.dart
@@ -6,5 +6,11 @@ const allIssuesKey = "ALL-ISSUES";
 const myIssuesKey = "MY-ISSUES";
 const likedIssuesKey = "LIKED-ISSUES";
 const serviceInItKey = "SERVICE_INIT";
-const baseUrl = 'http://192.168.1.105:8000/api';
-String socketUrl(int id) => "ws://192.168.1.107:8000/ws/comments/$id/";
+
+// prod
+const baseUrl = "https://reportit.as3hr.dev/api";
+String socketUrl(int id) => "ws://reportit.as3hr.dev/ws/comments/$id/";
+
+// dev
+// const baseUrl = 'http://192.168.1.105:8000/api';
+// String socketUrl(int id) => "ws://192.168.1.105:8000/ws/comments/$id/";
